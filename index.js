ce834ba23e2b85414d1db48fb53a79af862f0366
kim maul <kimberlymaul8@gmail.com>
2020-12-06 07:19:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = "Hello" + specialGuest + " let's talk about space";
 
-var topic = "space";
+var topic = "space";// 'Let's talk about space.';
 
 var conversation = `${topic}`; 
